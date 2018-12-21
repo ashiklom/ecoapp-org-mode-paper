25a4b5753994979b0f16ddbba2a9bfbe01ce04f2
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2018-12-21 14:29:09 -0500

    Remove analysis folder

--- analysis/R/try2fst.R
+++
@@ -1,8 +0,0 @@
-try2fst <- function(tryfiles, outfile) {
-  depends("fst", "data.table", "purrr")
-  stopifnot(all(file.exists(tryfiles)))
-
-  try_dt <- purrr::map(tryfiles, data.table::fread) %>%
-    data.table::rbindlist()
-  
-}
