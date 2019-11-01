7349e8993d686c549d1f25d9baab000bc7522b8a
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-11-01 11:15:07 -0400

    Old-new in track-changes

--- track-changes.sh
+++ track-changes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf shiklomanov_ea_diff.{aux,log,out,tex,pdf}
-latexdiff shiklomanov_ea_old.tex shiklomanov_ea.tex > shiklomanov_ea_diff.tex
+latexdiff shiklomanov_ea_old.tex shiklomanov_ea_new.tex > shiklomanov_ea_diff.tex
 pdflatex shiklomanov_ea_diff.tex
